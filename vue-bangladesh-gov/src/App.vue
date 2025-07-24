b3ca3b7838b28ee54da03a7117e30ea654595eeb
Abdullah al Mamun Bipu <ambipu@gmail.com>
2025-07-25 04:01:17 +0600

    Add vue-bangladesh-gov/src/App.vue 1753394477078

---
+++ vue-bangladesh-gov/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Header />
+    <Hero />
+    <Services />
+    <News />
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import Header from './components/Header.vue'
+import Hero from './components/Hero.vue'
+import Services from './components/Services.vue'
+import News from './components/News.vue'
+import Footer from './components/Footer.vue'
+</script>
